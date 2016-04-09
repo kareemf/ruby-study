85bc8dae83366aadaed674a3dc2d338f51973d5d
Kareem Francis <francis.kareem+git@gmail.com>
2016-04-09 00:13:58 -0400

    selection sort and spec

---
+++ lib/selection_sort.rb
@@ -0,0 +1,19 @@
+require_relative './utils'
+
+module Sorts
+  include Utils
+
+  def self.selection_sort(arr)
+    smallest_i = arr.index(arr.min)
+    Utils.swap(arr, 0, smallest_i)
+    # last index will be implicitly sorted
+    for i in 1...(arr.length-1)
+      smallest_i = i
+      for j in (i+1)...arr.length
+        smallest_i = j if arr[j] < arr[i]
+      end
+      Utils.swap(arr, i, smallest_i) if smallest_i != i
+    end
+    arr
+  end
+end
